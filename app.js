9f5be6b731338e76288583a01052c512457c8418
Kady <ku.ayush@yahoo.com>
2022-03-17 11:49:46 +0530

    uploading project to github

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const path = require('path-posix');
+const mongoose = require('mongoose');
+
+path.resolve(__dirname + 'foo');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String 
+});
+
+const user = new mongoose.model('User', userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.post("/register", function (req, res) {
+    const newUser= new user({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err) {
+        if(err) {
+            res.send(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function () {
+    console.log("Server is running on port: " + port);
+});
